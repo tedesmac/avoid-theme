6a0fdf21d983468a5669dd649371b6b1dd77fc7e
Kremor Pärt <hello@kremor.xyz>
2020-06-13 12:33:39 -0500

    Update inital files

--- index.php
+++ index.php
@@ -2,9 +2,44 @@
 
 <html lang="en">
   <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <?php wp_head();?>
   </head>
 
   <body>
-    <h1>Hello World</h1>
+    <nav></nav>
+
+    <section class="header"></section>
+
+    <main>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article>
+          <a>
+            <h1>
+              <?php the_title(); ?>
+            </h1>
+          </a>
+
+          <?php echo wp_trim_words( get_the_content(), 50 ); ?>
+
+          <a 
+            class="highlight"
+            href="<?php the_permalink(); ?>"
+            >
+            Read More
+          </a>
+
+          <hr />
+
+          <div class="extra">
+            <?php get_the_date(); ?>
+          </div>
+        </article>
+      <?php endwhile; endif; ?>
+    </main>
+
+    <?php wp_footer();?>
   </body>
 </html>
